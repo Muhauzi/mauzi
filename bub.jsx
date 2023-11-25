0a51fea26c9821dd0d589d8f6a1b7f4fe233c7cc
Ziphius-myth <115764458+Ziphius-myth@users.noreply.github.com>
2023-11-25 21:53:45 +0700

    Create bub.jsx

---
+++ bub.jsx
@@ -0,0 +1,85 @@
+bubimport React from "react";
+import "./style.css";
+
+export const Desktop = () => {
+  return (
+    <div className="desktop">
+      <div className="overlap-group-wrapper">
+        <div className="overlap-group">
+          <img className="flower" alt="Flower" src="flower-19.png" />
+          <img className="img" alt="Flower" src="flower-18.png" />
+          <img className="flower-2" alt="Flower" src="flower-17.png" />
+          <img className="flower-3" alt="Flower" src="flower-16.png" />
+          <img className="flower-4" alt="Flower" src="flower-15.png" />
+          <img className="flower-5" alt="Flower" src="flower-14.png" />
+          <img className="flower-6" alt="Flower" src="flower-13.png" />
+          <img className="flower-7" alt="Flower" src="flower-12.png" />
+          <img className="flower-8" alt="Flower" src="flower-11.png" />
+          <img className="flower-9" alt="Flower" src="flower-10.png" />
+          <img className="flower-10" alt="Flower" src="flower-09.png" />
+          <img className="flower-11" alt="Flower" src="flower-08.png" />
+          <img className="flower-12" alt="Flower" src="flower-07.png" />
+          <img className="flower-13" alt="Flower" src="flower-06.png" />
+          <img className="flower-14" alt="Flower" src="flower-05.png" />
+          <img className="flower-15" alt="Flower" src="flower-04.png" />
+          <img className="flower-16" alt="Flower" src="flower-03.png" />
+          <img className="flower-17" alt="Flower" src="flower-02.png" />
+          <img className="element" alt="Element" src="1.png" />
+          <img className="flower-18" alt="Flower" src="flower-01.png" />
+          <img className="element-2" alt="Element" src="2.png" />
+          <div className="rectangle" />
+          <p className="hello-my-girl-sorry">
+            Hello&nbsp;&nbsp;My Girl
+            <br />
+            Sorry im late send you this present
+          </p>
+          <p className="my-dearest-vioooo">
+            My Dearest Vioooo &lt;3
+            <br />
+            <br />
+            ,As I sit down to code this letter, my heart is overflowing with gratitude and love, much like the abundant
+            joy you have brought into my life. Today, on our anniversary, I find myself reflecting on the incredible
+            journey we have embarked upon together.
+            <br />
+            <br />
+            First and foremost, I want to express my deepest gratitude for choosing me. Your decision to walk alongside
+            me in this beautiful journey of love is a choice I cherish more than words can convey. Thank you for seeing
+            something special in us and for giving our love a chance to blossom into the extraordinary bond it is today.
+            <br />
+            <br />I am profoundly thankful for the second chance you have granted me. Your willingness to forgive,
+            understand, and grow together has been a testament to the strength of our connection. It is a gift I hold
+            close to my heart, and I promise to cherish and nurture this love we share.
+            <br />
+            <br />
+            Your unwavering effort and boundless patience have been the pillars that support the foundation of our
+            relationship. I am grateful for the times you&#39;ve stood by me, offering your support and understanding
+            even when the storms of life have tested us. Your commitment to us is the anchor that keeps us steady in the
+            ebb and flow of life.
+            <br />
+            <br />
+            And, of course, the thoughtful gift you bestowed upon me has left me utterly speechless. It is a tangible
+            representation of your love and consideration, and I am genuinely touched by the effort you put into
+            choosing something so meaningful. The fact that you know me so well and took the time to select a gift that
+            resonates with my heart speaks volumes about the depth of your love.
+            <br />
+            <br />
+            As I hold this token of your affection close, I am reminded of the sweetness and beauty that permeate our
+            relationship. Your love is a constant source of inspiration, and I feel incredibly fortunate to have you by
+            my side.
+            <br />
+            <br />
+            In every moment we&#39;ve shared and in every challenge we&#39;ve faced, your love has been my guiding
+            light. Thank you for being my confidant, my partner, and my greatest support. Here&#39;s to many more years
+            of laughter, love, and beautiful memories.
+            <br />
+            <br />
+            With all my love,
+            <br />
+            Your Boyfie &lt;3
+          </p>
+          <img className="element-3" alt="Element" src="3.png" />
+        </div>
+      </div>
+    </div>
+  );
+};
